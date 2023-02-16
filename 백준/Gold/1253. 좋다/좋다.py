950198ca0b6d00ac85e9ab37845348c742740e11
Sion <122588702+sionyy@users.noreply.github.com>
2023-02-17 02:08:40 +0900

    [Gold IV] Title: 좋다, Time: 1040 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -1,23 +1,24 @@
-N = int(input())
-NUM = sorted(map(int, input().split()))
-answer = 0
+N = int(input()) #수의 개수
+NUM = list(map(int, input().split())) # 예비 좋은수
+answer = 0 # 좋은수의 개수
+NUM.sort() # 예비 좋은 수 정렬
 
+for k in range(N):
+    check = NUM[k]
+    left = 0 #왼쪽수
+    right = N-1#오른쪽 수
 
-def abc(srt, target):
-    global answer
-    start, end = 0, len(srt) - 1
-    while start < end:
-        A = srt[start] + srt[end]
-        if target == A:
-            answer += 1
-            return
-        elif target > A:
-            start += 1
-        elif target < A:
-            end -= 1
-
-
-for i in range(N):
-    abc(NUM[:i] + NUM[i+1:], NUM[i])
-
+    while left<right:
+        if NUM[left] + NUM[right] == check:
+            if left != k and right !=k: #left,right !=일때 좋은 수 1 증가
+                answer+=1
+                break
+            elif left == k: 
+                left+=1 # 왼쪽값 오른쪽으로
+            elif right ==k:
+                right-=1 # 오른쪽 값 왼쪽으로
+        elif NUM[left] + NUM[right] < check:
+            left+=1
+        else:
+            right -=1
 print(answer)
