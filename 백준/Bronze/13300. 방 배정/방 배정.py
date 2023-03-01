6007ab677fe99e4a98293422680215b7369df730
Sion <122588702+sionyy@users.noreply.github.com>
2023-03-02 02:13:08 +0900

    [Bronze II] Title: 방 배정, Time: 136 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.py
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -11,16 +11,21 @@ for i in range(N):
         W[y]+=1
     else:
         M[y]+=1
-
 cnt=0
-for i in range(1,7):
+for i in range(7):
     if W[i] !=0 and W[i] <= K:
         cnt+=1
-    if M[i] !=0 and M[i] <= K:
-        cnt+=1
-    elif M[i] > K:
-        cnt+=M[i]//K+1
+    elif W[i] >= K and W[i]%K ==0:
+        cnt+=W[i]//K
     elif W[i] > K:
         cnt+=W[i]//K+1
+
+for i in range(7):
+    if M[i] !=0 and M[i] <= K:
+        cnt+=1
+    elif M[i] >= K and M[i]%K ==0:
+        cnt+=M[i]//K
+    elif M[i] > K:
+        cnt+=M[i]//K+1
 
 print(cnt)
