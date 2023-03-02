77294053e04e780173a039f0a7a6f45231d0f975
Sion <122588702+sionyy@users.noreply.github.com>
2023-03-02 17:07:55 +0900

    [Bronze I] Title: 색종이, Time: 484 ms, Memory: 121636 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,20 +1,18 @@
 # 횟수
 # x,y,너비,높이
 N=int(input())
-arr = [[0]*101 for _ in range(101)]
-
-for k in range(N): #y,x가 일반적이지만 반대여서 i,j위치 바꿈
+arr = [[0]*1001 for _ in range(1001)]
+for k in range(N): #y,x/i,j위치 바꿈
     x, y, m, n = map(int, input().split())
-    for j in range(m):
-        for i in range(n):
-            arr[y+i][x+j]=k+1 #더하는게 아니라 =으로 할당해야함
+    for i in range(y,y+n):
+        for j in range(x,x+m):
+            arr[i][j]=k+1
 
-full=[]
-for i in range(len(arr)): #arr을 1차원 배열로 만들기
-    full+=arr[i]          # == conunt 쉽게 하기 위해서
+result=[0]*(N+1)
+for i in range(1001):
+    for j in range(1001):
+        if arr[i][j]:
+            result[arr[i][j]]+=1
 
-cnt=0
-result=[]
-for j in range(1,N+1): #count 1부터 N까지를 출력하기
-    print(full.count(j))
-
+for i in range(1,N+1):
+    print(result[i])
