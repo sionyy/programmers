7d2a28cd38209f41086075e074f5468e766ef76d
Sion <122588702+sionyy@users.noreply.github.com>
2023-04-04 11:14:09 +0900

    [Silver II] Title: N과 M (9), Time: 212 ms, Memory: 118428 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,25 +1,20 @@
-N,M=map(int,input().split())
-card=list(map(int,input().split()))
-path=[0]*M
-used=[0]*N
-lst=[]
-
-def dfs(level):
+N,M = map(int,input().split())
+lst = list(map(int,input().split()))
+lst.sort()
+path = [0]*M
+used = [0]*N
+s = set()
+def abc(level):
     if level == M:
-        lst.append(tuple(path))
+        result = " ".join(map(str,path))
+        if result not in s:
+            s.add(result)
+            print(result)
         return
-
     for i in range(N):
-
-        if used[i]:
-            continue
-        used[i]=1
-        path[level]=card[i]
-        dfs(level+1)
-        used[i]=0
-
-dfs(0)
-
-new=sorted(set(lst))
-for i in range(len(new)):
-    print(*new[i])
+        if used[i] == 0:
+            path[level] = lst[i]
+            used[i] = 1
+            abc(level+1)
+            used[i] = 0
+abc(0)
