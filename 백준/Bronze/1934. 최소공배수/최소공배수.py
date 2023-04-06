1653826f2e2f9382a76a9c4081337856122bcb21
Sion <122588702+sionyy@users.noreply.github.com>
2023-04-06 17:49:15 +0900

    [Bronze I] Title: 최소공배수, Time: 4684 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,30 @@
+n = int(input())
+def soin(num):
+    lst = []
+    while num != 1:
+        for i in range(2, num + 1):
+            if num % i == 0:
+                num = num // i
+                lst.append(i)
+                break
+            else:
+                pass
+    return lst
+
+for repeat in range(n):
+    a, b = map(int, input().split())
+    lstA = soin(a)
+    lstB = soin(b)
+
+    full_lst=lstA+lstB
+
+    ans=[]
+    for i in range(len(full_lst)):
+        if full_lst[i] not in ans:
+            MAXX=max(lstA.count(full_lst[i]),lstB.count(full_lst[i]))
+            for k in range(MAXX):
+                ans.append(full_lst[i])
+    result=1
+    for i in range(len(ans)):
+        result*=ans[i]
+    print(result)
