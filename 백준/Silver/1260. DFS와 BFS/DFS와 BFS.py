6d3a7da608db127d25c958f8c4cac9dd909432ce
Sion <122588702+sionyy@users.noreply.github.com>
2023-04-10 14:29:08 +0900

    [Silver II] Title: DFS와 BFS, Time: 464 ms, Memory: 32372 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -0,0 +1,40 @@
+N,M,V=map(int,input().split())
+adj=[[] for _ in range(N+1)]
+for _ in range(M):
+    s,e=map(int,input().split())
+    adj[s].append(e)
+    adj[e].append(s)
+for i in range(N+1):
+    adj[i].sort()
+
+def dfs(n):
+    ans_dfs.append(n)
+    visit[n]=1
+    for i in adj[n]:
+        if visit[i]==0:
+            dfs(i)
+
+def bfs(n):
+    q=[]
+    q.append(n)
+    ans_bfs.append(n)
+    visit[n]=1
+
+    while q:
+        now=q.pop(0)
+        for i in adj[now]:
+            if not visit[i]:
+                q.append(i)
+                ans_bfs.append(i)
+                visit[i]=1
+
+
+visit = [0]*(N+1)
+ans_dfs = []
+dfs(V)
+print(*ans_dfs)
+visit = [0]*(N+1)
+
+ans_bfs=[]
+bfs(V)
+print(*ans_bfs)
