9d0711c4a5897946a0486a33a8db0badc6574439
Sion <122588702+sionyy@users.noreply.github.com>
2023-04-10 16:23:04 +0900

    [Silver III] Title: 바이러스, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,22 @@
+N=int(input())
+M=int(input())
+adj=[[] for _ in range(N+1)]
+for i in range(M):
+    s,e=map(int,input().split())
+    adj[s].append(e)
+    adj[e].append(s)
+
+
+def dfs(n):
+    if visit[n]==1:
+        return
+    go.append(n)
+    visit[n]=1
+    for i in adj[n]:
+        dfs(i)
+
+
+visit=[0]*(N+1)
+go=[]
+dfs(1)
+print(len(go)-1)
