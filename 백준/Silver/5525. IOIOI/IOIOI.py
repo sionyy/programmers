93ea04339514a184720adc578e9ad8f57b229917
Sion <122588702+sionyy@users.noreply.github.com>
2023-04-17 17:46:43 +0900

    [Silver I] Title: IOIOI, Time: 196 ms, Memory: 122804 KB -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,14 @@
+import sys
+input=sys.stdin.readline
+
+N=int(input())
+s=int(input())
+st=list(input())
+
+first = ['I']+['O','I']*N
+
+cnt=0
+for i in range(len(st)-len(first)+1):
+    if first ==st[i:i+len(first)]:
+        cnt+=1
+print(cnt)
