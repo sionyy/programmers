a208cfec615f8008963feed635f6f5281d7cb6c3
Sion <122588702+sionyy@users.noreply.github.com>
2023-04-19 16:03:01 +0900

    [Silver I] Title: 숨바꼭질, Time: 1224 ms, Memory: 38960 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,20 @@
+def bfs(s, e):
+    q = []
+    v = [0]*200001
+
+    q.append(s)
+    v[s] = 1
+
+    while q:
+        c = q.pop(0)
+        if c == e:
+            return v[e]-1
+        for n in (c-1, c+1, c*2):
+            if 0<=n<=200000 and v[n]==0:
+                q.append(n)
+                v[n] = v[c]+1
+    return -1
+
+N, K = map(int, input().split())
+ans = bfs(N, K)
+print(ans)
