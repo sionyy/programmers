8dcfcd8378f924482115282d97c166e33f13c8d6
Sion <122588702+sionyy@users.noreply.github.com>
2023-04-19 16:06:32 +0900

    [Silver I] Title: 숨바꼭질, Time: 132 ms, Memory: 38960 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,20 +1,22 @@
-def bfs(s, e):
-    q = []
-    v = [0]*200001
+from collections import deque
+def bfs(s,e):
+    q=deque()
+    v=[0]*200001
 
     q.append(s)
-    v[s] = 1
+    v[s]=1
 
     while q:
-        c = q.pop(0)
+        c=q.popleft()
         if c == e:
             return v[e]-1
-        for n in (c-1, c+1, c*2):
+
+        for n in (c-1,c+1,c*2):
             if 0<=n<=200000 and v[n]==0:
                 q.append(n)
-                v[n] = v[c]+1
+                v[n]=v[c]+1
     return -1
 
-N, K = map(int, input().split())
-ans = bfs(N, K)
+N,K=map(int,input().split())
+ans=bfs(N,K)
 print(ans)
