fca93faf961419eb3583c6f1553e05186f02054d
Sion <122588702+sionyy@users.noreply.github.com>
2023-07-06 17:20:50 +0900

    [Gold V] Title: 블랙 프라이데이, Time: 44 ms, Memory: 31768 KB -BaekjoonHub

--- 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.py
+++ 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.py
@@ -2,21 +2,21 @@ import sys
 input = sys.stdin.readline
 
 def solution(now):
-    N, target = map(int, input().split())
-    lst = sorted(list(map(int, input().split())))
-    box = set([0] + lst) # 중요
-    if target in box: #한개
+    global N,target,lst
+    box = set([0] + lst)
+    if target in box:
         return 1
-    while now < N-1: #끝까지 탐색
-        left, right = lst[now], lst[N-1] #left, right 설정
-        new_target = target-left-right
-        #set(0)을 추가하여 2개와 3개를 동시에 검색
+    while now < N-1:
+        left, right = lst[now], lst[N-1]
+        new_target = target - left - right
         if new_target != left and new_target != right and new_target in box:
             return 1
         if new_target < 0:
-            N-=1
+            N -= 1
         else:
-            now+=1
+            now += 1
     return 0
 
+N, target = map(int, input().split())
+lst = sorted(list(map(int, input().split())))
 print(solution(0))
